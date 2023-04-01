10d96dac83f4f21800fdae648c374284aa26fff1
nihelkb <nihelkella@gmail.com>
2023-04-01 02:15:26 +0200

    Added logging messages

---
+++ src/main/java/es/upm/pproject/miniproject/UniversityManagement.java
@@ -0,0 +1,152 @@
+package es.upm.pproject.miniproject;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class UniversityManagement {
+    private Map<Integer, Course> courseList;
+    private Map<Integer, Student> studentsList;
+    
+    private static final Logger logger = LoggerFactory.getLogger(UniversityManagement.class);
+
+    public UniversityManagement(){
+        courseList = new HashMap<Integer, Course>();
+        studentsList = new HashMap<Integer, Student>();
+    }
+
+    public void addStudentToSystem(int id, String name, String email) throws UnivSystemException{
+        
+        logger.info("Registering student with id={}, name={}, email={} ...", id, name, email);
+        
+        if(name != null && name.length() == 0){
+            logger.error("The name introduced is blank");
+            throw new UnivSystemException("Name field cannot be blank");
+        }
+
+        if(email != null && email.length() == 0){
+            logger.error("The email introduced is blank");
+            throw new UnivSystemException("Mail field cannot be blank");
+        }
+
+        if(email != null && !(email.contains("@") && email.charAt(email.length() - 1) != '.')){
+            logger.error("The email address format is not correct");
+            throw new UnivSystemException("The format of the email address must be correct");
+        }
+
+        Student student = new Student(id, name, email);
+        studentsList.put(id, student); 
+        logger.info("Registering of the student with id={}, name={}, email={} completed", id, name, email);
+
+    }
+
+    public void addCourseToSystem(int code, String name, String coordinator) throws UnivSystemException{
+
+        logger.info("Registering course with id={}, name={}, email={} ...", code, name, coordinator);
+
+        if(name != null && name.length() == 0){
+            logger.error("The name introduced is blank");
+            throw new UnivSystemException("Name field cannot be blank");
+        }
+
+        if(coordinator != null && coordinator.length() == 0){
+            logger.error("The coordinator introduced is blank");
+            throw new UnivSystemException("Coordinator field cannot be blank");
+        }
+
+        Course course = new Course(code, name, coordinator);
+        courseList.put(code, course);
+        logger.info("Registering of the course with id={}, name={}, email={} completed", code, name, coordinator);
+
+    }
+
+    public void enrollStudent(int idStudent, int courseCode) throws UnivSystemException{
+
+        logger.info("Enrolling the student with id={} in course with code={} ...", idStudent, courseCode);
+
+        Student student = studentsList.get(idStudent);
+        Course course = courseList.get(courseCode);
+
+        if(!studentsList.containsKey(idStudent)){
+            logger.error("The student with id={} is not registered in the system", idStudent);
+            throw new UnivSystemException("The student must be already registered in the system.");
+        }
+
+        if(!courseList.containsKey(courseCode)){
+            logger.error("The course with code={} is not registered in the system", courseCode);
+            throw new UnivSystemException("The course must be already registered in the system.");
+        }
+
+        if(course.getNumberStudents() >= 50){
+            logger.error("The course with code={} has not more places available", courseCode);
+            throw new UnivSystemException("Mo more places are available for the course");
+        }
+
+        if(course.isInCourse(student)){
+            logger.error("The student with id={} is already enrolled in the course", idStudent);
+            throw new UnivSystemException("A student cannot be enrolled in the same course twice");
+        }
+
+        course.enrollStudent(student);
+        logger.info("The student with id={} has been succesfully enrolled in course with code={}", idStudent, courseCode);
+    }
+    
+    public void unenrollStudent(int idStudent, int courseCode) throws UnivSystemException{
+
+        logger.info("Unenrolling the student with id={} from course with code={} ...", idStudent, courseCode);
+
+        Student student = studentsList.get(idStudent);
+        Course course = courseList.get(courseCode);
+
+        if(!studentsList.containsKey(idStudent)){
+            logger.error("The student with id={} is not registered in the system", idStudent);
+            throw new UnivSystemException("The student must be already registered in the system.");
+        }
+
+        if(!courseList.containsKey(courseCode)){
+            logger.error("The course with code={} is not registered in the system", courseCode);
+            throw new UnivSystemException("The course must be already registered in the system.");
+        }
+
+        if(!course.isInCourse(student)){
+            logger.error("The student with id={} is not enrolled in the course", idStudent);
+            throw new UnivSystemException("The student is not enrolled in the course.");
+        }
+
+        course.unenrollStudent(student);
+        logger.info("The student with id={} has been succesfully unenrolled from course with code={}", idStudent, courseCode);
+    }
+
+    public List<Student> matriculatedStudentsList(int courseCode) throws UnivSystemException{
+        logger.info("Loading list of students matriculated in course with id={}...", courseCode);
+        Course course = courseList.get(courseCode);
+        List<Student> students = course.getStudentsList();
+        logger.debug("Sorting list by identification number...");
+        students.sort(Comparator.comparing(Student::getId));
+        logger.info("List of students matriculated in course with id={} ready to display", courseCode);
+        return students;
+    }
+
+    public List<Student> registeredStudentsList(){
+        logger.info("Loading list of students registered...");
+        List<Student> students = new ArrayList<>(studentsList.values());
+        logger.debug("Sorting list by identification number...");
+        students.sort(Comparator.comparing(Student::getId));
+        logger.info("List of students registered ready to display");
+        return students;
+    }
+
+    public List<Course> registeredCoursesList(){
+        logger.info("Loading list of courses registered...");
+        List<Course> courses = new ArrayList<>(courseList.values());
+        logger.debug("Sorting list by code...");
+        courses.sort(Comparator.comparing(Course::getCode));
+        logger.info("List of courses registered ready to display");
+        return courses;
+    }
+
+}
